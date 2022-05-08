76766086135b8617abdb742fe79a452551599dd8
victoria makena <makenavictoria1@gmail.com>
2022-05-08 16:21:06 +0300

    initiate user model

--- app/models.py
+++ app/models.py
@@ -1,2 +1,11 @@
-class Role(db.Model):
-    
+from . import db
+
+class User(db.Model):
+    #model to create new users
+    __tablename__='users'
+
+    identity=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username} '
