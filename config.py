a96bdb61f8ede0ad954fb000496f958f00522b9f
victoria makena <makenavictoria1@gmail.com>
2022-05-09 00:59:29 +0300

    add email configuration

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
+import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://elvis:moraaelvis@localhost/pitch'
+
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     pass
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MAIL_USERNAME='makenavictoria1@gmail.com'
+export MAIL_PASSWORD='golden5626era.?!'
+python3.9 manage.py server
