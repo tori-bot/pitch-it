1cfd1cca48f61f74c320e1534ddb213ffdb86f60
victoria makena <makenavictoria1@gmail.com>
2022-05-08 21:24:42 +0300

    add functions to validate email and username during registration

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField,ValidationError
 from wtforms.validators import InputRequired,Email,EqualTo
 from ..models import User
+
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[InputRequired(),Email()])
@@ -9,3 +10,11 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
