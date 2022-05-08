1ddb1ee7460c98d46bee05ae58e27e89e380dbf4
victoria makena <makenavictoria1@gmail.com>
2022-05-08 16:37:38 +0300

    add relationship between user and role tables

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
 
     identity=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(255))
+    role_id=db.Column(db.Integer,db.ForeignKey('roles.identity'))
 
     def __repr__(self):
         return f'User {self.username} '
