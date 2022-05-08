441a0dadf5340443416e0a5136cf6912256a1db1
victoria makena <makenavictoria1@gmail.com>
2022-05-08 17:26:31 +0300

    generate password hash

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from turtle import backward
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     #model to create new users
@@ -8,6 +9,19 @@ class User(db.Model):
     identity=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(255))
     role_id=db.Column(db.Integer,db.ForeignKey('roles.identity'))
+    pass_secure=db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username} '
@@ -23,3 +37,12 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'User {self.name} '
+
+# class Comment(db.Model):
+#     __tablename__='comment'
+
+#     identity=db.Column(db.Integer,primary_key=True)
+#     pitch_id=db.Column(db.Integer)
+#     pitch_title=db.Column(db.String)
+#     pitch_comment=db.Column(db.String)
+#     posted=db.Column(db.DateTime,default=datetime.utcnow)
