059dcd01c4c855968aea075fcc74c19fb1a05b3f
victoria makena <makenavictoria1@gmail.com>
2022-05-08 20:36:03 +0300

    register auth blueprint in create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -15,4 +15,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
+
     return app
