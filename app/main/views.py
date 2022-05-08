37af05be964bc1795e0345c24ce8549a1447d98e
victoria makena <makenavictoria1@gmail.com>
2022-05-09 00:50:34 +0300

    add photo upload view function

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from flask import render_template,request,redirect,url_for,abort
 from flask_login import login_required
 from . import main
 from .forms import UpdateProfile,CommentForm
-from .. import db
+from .. import db,photos
 
 @main.route('/')
 def index():
@@ -37,6 +37,19 @@ def update_profile(uname):
 
         return redirect(url_for('.profile',uname=user.username))
     return render_template('profile/update.html',form=form)
+
+@main.route('/user/<uname>/update/pic',methods=['POST'])
+@login_required
+def update_pic(uname):
+    user= User.query.filter_by(username = uname).first()
+
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_url = path
+        db.session.commit()
+        
+    return redirect(url_for('main.profile',uname=uname))
 
 # @main.route('/comment',methods=['GET','POST'])
 # @login_required
