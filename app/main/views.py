5a7691a7d8459a441a354b9c5fc24531c3b42caa
victoria makena <makenavictoria1@gmail.com>
2022-05-08 23:20:31 +0300

    add index view and profile view

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,22 @@
-from crypt import methods
+from django.shortcuts import render
+from ..models import User,Comment
+from flask import render_template,request,redirect,url_for,abort
 from flask_login import login_required
+from . import main
+
+@main.route('/')
+def index():
+    title='Pitch it...One minute could change your life.'
+    return render_template('index.html',title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user=User.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html',user=user)
 
 # @main.route('/comment',methods=['GET','POST'])
 # @login_required
