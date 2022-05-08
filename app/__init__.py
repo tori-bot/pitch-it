dc0c7969d3162c8748dc0cfa0c2d5c259823bcbf
victoria makena <makenavictoria1@gmail.com>
2022-05-08 16:15:41 +0300

    create app instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask_bootstrap import Bootstrap
+from config import config_options
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 bootstrap=Bootstrap()
 db=SQLAlchemy()
+
+def create_app(config_name):
+    #create flask app instance
+    app=Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    return app
