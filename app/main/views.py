47998fd8e74102b6efe85dec063ce7d6f0f8d0f9
victoria makena <makenavictoria1@gmail.com>
2022-05-09 00:06:36 +0300

    add view function to update profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from crypt import methods
 from ..models import User,Comment
 from flask import render_template,request,redirect,url_for,abort
 from flask_login import login_required
 from . import main
+from .forms import UpdateProfile,CommentForm
+from .. import db
 
 @main.route('/')
 def index():
@@ -18,7 +20,23 @@ def profile(uname):
 
     return render_template('profile/profile.html',user=user)
 
+@main.route('/user/<uname>/update',methods=['GET','POST'])
+def update_profile(uname):
+    user=User.query.filter_by(username=uname).first()
 
+    if user is None:
+        abort(404)
+    
+    form=UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio=form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+    return render_template('profile/update.html',form=form)
 
 # @main.route('/comment',methods=['GET','POST'])
 # @login_required
