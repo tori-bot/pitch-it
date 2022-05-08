1d11b08fec11c4a04e2cb1bdab0fad74a1faea80
victoria makena <makenavictoria1@gmail.com>
2022-05-09 01:19:02 +0300

    add welcome email message

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from ..models import User
 from . import auth
 from .forms import RegistrationForm,LoginForm
 from .. import db
+from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -36,6 +37,9 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message(' Welcome to Pitch It.','email/welcome_user',user.email,user=user)
+
         return redirect(url_for('auth.login'))
         
     return render_template('auth/register.html',title=title,registration_form = form)
