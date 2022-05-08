30571388cecfa31f802a21bfd2b7112c3e1b12f3
victoria makena <makenavictoria1@gmail.com>
2022-05-08 16:28:48 +0300

    create app instance and instanciate manager class

--- manage.py
+++ manage.py
@@ -1,5 +1,11 @@
 from app import create_app,db
 from app.models import User
+from flask_script import Manager,Server
+
+app=create_app('development')
+manager=Manager(app)
+manager.add_command('server',Server)
+
 
 @manager.shell
 def make_shell_context():
