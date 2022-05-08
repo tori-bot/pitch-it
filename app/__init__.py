5d97d0fa4baefb09d7426865f4e82a0cbcbb5dc5
victoria makena <makenavictoria1@gmail.com>
2022-05-09 00:20:19 +0300

    configure photo uploads

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,26 @@
+from re import U
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 login_manager=LoginManager()
-login_manager.sessio='strong'
+login_manager.session='strong'
 login_manager.login_view='auth.login'
 
 bootstrap=Bootstrap()
 db=SQLAlchemy()
+photos=UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     #create flask app instance
     app=Flask(__name__)
 
     app.config.from_object(config_options[config_name])
+
+    configure_uploads(app,photos)
 
     bootstrap.init_app(app)
     db.init_app(app)
