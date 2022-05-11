fe68f54cb2dfe58440973e2e1c6734bb032d3574
victoria makena <makenavictoria1@gmail.com>
2022-05-11 06:44:24 +0300

    fix bug and prepare for deployment

--- app/main/views.py
+++ app/main/views.py
@@ -26,11 +26,11 @@ def index():
 @main.route('/user/<uname>')
 def profile(uname):
     user=User.query.filter_by(username=uname).first()
-    pitches=Pitch.query.filter_by(user_id=current_user.id).all()
+    
     if user is None:
         abort(404)
 
-    return render_template('/profile/profile.html',user=user,pitches=pitches)
+    return render_template('/profile/profile.html',user=user)
 
 @main.route('/pitch/new',methods=['GET','POST'])
 @login_required
